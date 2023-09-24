4da91391d060b442e0ffd98a312ba120688c2437
Michael Hardy <mbhardy001@gmail.com>
2023-09-24 16:27:31 -0400

    Created model_visualization module with plotting routines & folder with model plots. Added routines in model_io to load models and save plots. Removed utils/model_plt.py.

--- utils/model_io.py
+++ utils/model_io.py
@@ -2,9 +2,9 @@ from datetime import datetime
 import pickle
 import os
 
-def save_model_metrics(all_model_metrics, dirpath, model):
+def save_model_metrics(all_model_metrics, dir_path, model):
     formatted_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M")
-    timestamp_folder = f"{dirpath}/{model}_{formatted_datetime}"
+    timestamp_folder = os.path.join(dir_path, f"{model}_{formatted_datetime}")#f"{dir_path}/{model}_{formatted_datetime}"
     os.makedirs(timestamp_folder)
 
     for model_dict in all_model_metrics:
@@ -13,8 +13,25 @@ def save_model_metrics(all_model_metrics, dirpath, model):
         with open(filepath, 'wb') as file:
             pickle.dump(model_dict, file)
 
-def load_model_metrics(dirpath, filename):
-    filepath = f"{dirpath}/{filename}"
+def save_plots(figs_dict, plots_dir_path, metrics_dir_path):
+    formatted_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    corresponding_metrics_folder = os.path.basename(metrics_dir_path)
+    timestamp_folder = os.path.join(plots_dir_path, f"{corresponding_metrics_folder}_({formatted_datetime})")
+    print(timestamp_folder)
+    os.makedirs(timestamp_folder)
+    for metric, fig in figs_dict.items():
+        fig.savefig(os.path.join(timestamp_folder, metric))
+    
+def load_model_metrics(dir_path, filename):
+    filepath = os.path.join(dir_path, filename)
     with open(filepath, 'rb') as file:
         model_metrics = pickle.load(file)
     return model_metrics
+
+def load_model_metrics_directory(dir_path):
+    all_model_metrics = []
+    for filename in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, filename)):
+            model_metrics = load_model_metrics(dir_path, filename)
+            all_model_metrics.append(model_metrics)
+    return all_model_metrics
